5c23454575b45c4013768688b5c654e4955d7b1e
Apothe0s1s <118425534+Apothe0s1s@users.noreply.github.com>
2022-11-28 13:30:52 +0300

    HW2

---
+++ HW_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10. Показывает 2-ю цифру 3-значного числа.
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+if ((N<1000) && (N>99))
+    Console.WriteLine("Вторая цифра " + (N/10)% 10);    
+else
+    Console.WriteLine("Не трехзначное число");
---
+++ HW_15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 15. Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day > 5 && day < 8)
+    Console.WriteLine("Да");
+if(day > 0 && day < 6)
+    Console.WriteLine("Нет");
+if(day < 1 || day > 7)
+    Console.WriteLine("Не день недели");
